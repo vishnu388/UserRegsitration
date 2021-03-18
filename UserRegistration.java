0ff449ce40603686e660c425cb52e99d88618d05
vishnu <vishnu388@gmail.com>
2021-03-18 17:39:10 +0530

    [vishnu]:Added to LowerCase Letter

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class UserRegistration {
-	public boolean validateUpperCase(String UpperCase) {
-		String regExp = "^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$";
+	public boolean validateLowerCase(String LowerCase) {
+		String regExp = "^(?=.*?[a-z])(?=.*?[A-Z]).{8,}$";
 		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher=pattern.matcher(UpperCase);
+		Matcher matcher=pattern.matcher(LowerCase);
 		return matcher.matches();
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter your UpperLetter");
-		String Upper=sc.next();
-		boolean result=new UserRegistration().validateUpperCase(Upper);
+		System.out.println("Enter your LowerLetter");
+		String Lower=sc.next();
+		boolean result=new UserRegistration().validateLowerCase(Lower);
 		System.out.println(result);
 
 	}

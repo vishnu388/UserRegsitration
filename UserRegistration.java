a1a1610877f41d815aae016fe0d4758372b0addd
vishnu <vishnu388@gmail.com>
2021-03-18 17:52:14 +0530

    [vishnu}:Added to Numeric Values

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class UserRegistration {
-	public boolean validateLowerCase(String LowerCase) {
-		String regExp = "^(?=.*?[a-z])(?=.*?[A-Z]).{8,}$";
+	public boolean validateNumericNumber(String NumericNumber) {
+		String regExp = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}$";
 		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher=pattern.matcher(LowerCase);
+		Matcher matcher=pattern.matcher(NumericNumber);
 		return matcher.matches();
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter your LowerLetter");
-		String Lower=sc.next();
-		boolean result=new UserRegistration().validateLowerCase(Lower);
+		System.out.println("Enter your NumericLetter");
+		String Numeric=sc.next();
+		boolean result=new UserRegistration().validateNumericNumber(Numeric);
 		System.out.println(result);
 
 	}

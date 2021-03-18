be7168b0892983100bcf620c6d2daffc8e2974a2
vishnu <vishnu388@gmail.com>
2021-03-18 14:38:59 +0530

    [vishnu]:Added to Enter a valid Password

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,21 +4,22 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class UserRegistration {
-	public boolean validateMobileNumber(String MobileNumber) {
-		String regExp = "^[0-9]{2}[-][0-9]{10}$";
+	public boolean validatePassword(String Password) {
+		String regExp = "[a-zA-Z[@]0-9]{8,}";
 		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher=pattern.matcher(MobileNumber);
+		Matcher matcher=pattern.matcher(Password);
 		return matcher.matches();
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter your MobileNumber");
-		String MobileNumber=sc.next();
-		boolean result=new UserRegistration().validateMobileNumber(MobileNumber);
+		System.out.println("Enter your Password");
+		String Password=sc.next();
+		boolean result=new UserRegistration().validatePassword(Password);
 		System.out.println(result);
-
 	}
 
 }
+
+
 
 

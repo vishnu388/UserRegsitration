890660525652e522d7af04c8a8defcadcfb61092
vishnu <vishnu388@gmail.com>
2021-03-18 17:31:07 +0530

    [vishnu]:Added to UpperCase Latter

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,18 +4,19 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class UserRegistration {
-	public boolean validatePassword(String Password) {
-		String regExp = "[a-zA-Z[@]0-9]{8,}";
+	public boolean validateUpperCase(String UpperCase) {
+		String regExp = "^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$";
 		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher=pattern.matcher(Password);
+		Matcher matcher=pattern.matcher(UpperCase);
 		return matcher.matches();
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter your Password");
-		String Password=sc.next();
-		boolean result=new UserRegistration().validatePassword(Password);
+		System.out.println("Enter your UpperLetter");
+		String Upper=sc.next();
+		boolean result=new UserRegistration().validateUpperCase(Upper);
 		System.out.println(result);
+
 	}
 
 }

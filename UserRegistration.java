9aa0288006d36f2a3f34ca857359258ec8e56485
vishnu <vishnu388@gmail.com>
2021-03-18 12:43:24 +0530

    [vishnu]:Added to Enter Valid MobileNumber

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,20 +4,21 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class UserRegistration {
-	public boolean validateEmail(String Email) {
-		String regExp = "^[a-zA-Z0-9]+([.+_-]{0,1}+[a-zA-Z0-9])*+[@]+[a-zA-Z]+[.]+[a-zA-Z]{2,4}+([.]{1}+[a-zA-Z]{0,2})*";
+	public boolean validateMobileNumber(String MobileNumber) {
+		String regExp = "^[0-9]{2}[-][0-9]{10}$";
 		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher=pattern.matcher(Email);
+		Matcher matcher=pattern.matcher(MobileNumber);
 		return matcher.matches();
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter your Email");
-		String Email=sc.next();
-		boolean result=new UserRegistration().validateEmail(Email);
+		System.out.println("Enter your MobileNumber");
+		String MobileNumber=sc.next();
+		boolean result=new UserRegistration().validateMobileNumber(MobileNumber);
 		System.out.println(result);
 
 	}
 
 }
+
 

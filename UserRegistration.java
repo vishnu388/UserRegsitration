8987b7a5f499484ee1b75142eaa4997e0d75ebf8
vishnu <vishnu388@gmail.com>
2021-03-18 12:30:49 +0530

    [vishnu]: Added to Enter valid Email

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,19 +4,20 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class UserRegistration {
-	public boolean validateLastName(String LastName) {
-		String regExp = "[A-Z][A-Za-z]{3,}";
+	public boolean validateEmail(String Email) {
+		String regExp = "^[a-zA-Z0-9]+([.+_-]{0,1}+[a-zA-Z0-9])*+[@]+[a-zA-Z]+[.]+[a-zA-Z]{2,4}+([.]{1}+[a-zA-Z]{0,2})*";
 		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher=pattern.matcher(LastName);
+		Matcher matcher=pattern.matcher(Email);
 		return matcher.matches();
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter your LastName");
-		String LastName=sc.next();
-		boolean result=new UserRegistration().validateLastName(LastName);
+		System.out.println("Enter your Email");
+		String Email=sc.next();
+		boolean result=new UserRegistration().validateEmail(Email);
 		System.out.println(result);
 
 	}
 
 }
+

f64aa813d93354e4be74a0c11c3eeef8bedb0b9f
vishnu <vishnu388@gmail.com>
2021-03-18 18:16:12 +0530

    [vishnu]:Added to SpecialCharacter

--- UserRegistration.java
+++ UserRegistration.java
@@ -4,22 +4,23 @@ import java.util.Scanner;
 import java.util.regex.*;
 
 public class UserRegistration {
-	public boolean validateNumericNumber(String NumericNumber) {
-		String regExp = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}$";
+	public boolean validateSpecialCharacter(String SpecialCharacter) {
+		String regExp = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}#";
 		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher=pattern.matcher(NumericNumber);
+		Matcher matcher=pattern.matcher(SpecialCharacter);
 		return matcher.matches();
 	}
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter your NumericLetter");
-		String Numeric=sc.next();
-		boolean result=new UserRegistration().validateNumericNumber(Numeric);
+		System.out.println("Enter your SpecialCharacter");
+		String SPCharacter=sc.next();
+		boolean result=new UserRegistration().validateSpecialCharacter(SPCharacter);
 		System.out.println(result);
 
 	}
 
 }
+
 
 
 

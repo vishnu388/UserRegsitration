7833145e17dd89c5c342f04132a055cca8e6ac75
vishnu <vishnu388@gmail.com>
2021-03-24 13:29:13 +0530

    [vishnu]:Added to Exceptins

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,77 +1,52 @@
-package UserValid;
-
-import java.util.Scanner;
-import java.util.regex.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class UserRegistration {
-	public boolean validateFirstName(String FirstName) {
-		String regExp = "[A-Z][A-Za-z]{3,}";
-		Pattern pattern = Pattern.compile(regExp);   //uc-1
-		Matcher matcher=pattern.matcher(FirstName);
-		return matcher.matches();
-	}
-	public boolean validateLastName(String LastName) {
-		String regExp = "[A-Z][A-Za-z]{3,}";
-		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher=pattern.matcher(LastName);    //uc-2
-		return matcher.matches();
-	}
-	public boolean validateEmail(String Email) {
-		String regExp = "^[a-zA-Z0-9]+([.+_-]{0,1}+[a-zA-Z0-9])*+[@]+[a-zA-Z]+[.]+[a-zA-Z]{2,4}+([.]{1}+[a-zA-Z]{0,2})*";
-		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher=pattern.matcher(Email);    //uc-3
-		return matcher.matches();
-	}
-	public boolean validateMobileNumber(String MobileNumber) {
-		String regExp = "^[0-9]{2}[-][0-9]{10}$";
-		Pattern pattern = Pattern.compile(regExp);     //uc-4
-		Matcher matcher=pattern.matcher(MobileNumber);
-		return matcher.matches();
-	}
-	public boolean validatePassword(String Password) {
-		String regExp = "[a-zA-Z[@]0-9]{8,}";
-		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher=pattern.matcher(Password);     //uc-5
-		return matcher.matches();
-	}
-	public boolean validateUpperCase(String UpperCase) {
-		String regExp = "^(?=.*?[A-Z])(?=.*?[a-z]).{8,}$";
-		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher=pattern.matcher(UpperCase);    //uc-6
-		return matcher.matches();
-	}
-	public boolean validateNumericNumber(String NumericNumber) {
-		String regExp = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}$";
-		Pattern pattern = Pattern.compile(regExp);       //uc-7
-		Matcher matcher=pattern.matcher(NumericNumber);
-		return matcher.matches();
-	}
-	public boolean validateSpecialCharacter(String SpecialCharacter) {
-		String regExp = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).{8,}#";
-		Pattern pattern = Pattern.compile(regExp);        //uc-8
-		Matcher matcher=pattern.matcher(SpecialCharacter);
-		return matcher.matches();
-	}
-	public boolean validateCheckEmail(String CheckEmail) {
-		String regExp = "^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-z0-9]+.[a-zA-Z]{2}([.][a-zA-Z]{2,4})?$";
-		Pattern pattern = Pattern.compile(regExp);
-		Matcher matcher=pattern.matcher(CheckEmail);    //uc-9
-		return matcher.matches();
-	}
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter your valid Emails");
-		String Email=sc.next();
-		boolean result=new UserRegistration().validateCheckEmail(Email);
-		System.out.println(result);
-		
-		
-		
-	}
+    public static final String firstNamePattern = "^[A-Z][a-z]{2,}";
+    public static final String lastNamePattern = "^[A-Z][a-z]{2,}";
+    public static final String emailPattern = "^[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-z0-9]+.[a-zA-Z]{2,4}(.[a-zA-Z]{2,4})$";
+    public static final String mobileNumberPattern = "^[0-9]{2}[-][0-9]{10}$";
+    public static final String passwordPattern = "^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#$%^&)*(-+)]).{8,}$";
+    public static boolean validatePattern(String usrInput, String regExp) throws UserExceptions {
+        try {
+            Pattern pattern = Pattern.compile(regExp);
+            Matcher matcher = pattern.matcher(usrInput);
+            return matcher.matches();
+        }catch (NullPointerException e){
+            throw new UserExceptions("Enter valid input which is not null.");
+        }
+    }
+    public static void ifValidOutputAfterTest(String regExpString, String[] testSamples){
+        for (String testString : testSamples) {
+            try {
+                if (validatePattern(testString, regExpString)) {
+                    System.out.printf(" Valid  : %s\n", testString);
+                } else
+                    System.out.printf("Invalid : %s\n", testString);
+            } catch (UserExceptions ue) {
+                System.out.println(ue.getMessage());
+            }
+        }
 
+    }
+
+    public static void main(String args[]) {
+        String[] firstNameList= {"Vishnu","Vishn", "vishnu", null, ""};
+        ifValidOutputAfterTest(firstNamePattern, firstNameList);
+
+        String[] lastNameList= {"Vardhan", "Vardha", "vardhan"};
+        ifValidOutputAfterTest(lastNamePattern, lastNameList);
+
+        String[] emailList= {"abc@yahoo.com" ,"abc-100@yahoo.com" ,"abc.100@yahoo.com", "abc111@abc.com", "abc-100@abc.net",
+                "abc.100@abc.com.au", "abc@1.com", "abc@gmail.com.com", "abc+100@gmail.com", "a.b1.00@abc.net", "abc", "abc@.com.my",
+                "abc123@gmail.a", "abc123@.com", "abc123@.com.com", ".abc@abc.com", "abc()*@gmail.com", "abc@%*.com",
+                "abc..2002@gmail.com", "abc.@gmail.com", "abc@abc@gmail.com", "abc@gmail.com.1a", "abc@gmail.com.aa.au"};
+        ifValidOutputAfterTest(emailPattern, emailList);
+
+        String[] mobileNumberList= {"91-8106261925", "8106261925"};
+        ifValidOutputAfterTest(mobileNumberPattern, mobileNumberList);
+
+        String[] passwordList= {"A.vishnu@38", "avishnu@388"};
+        ifValidOutputAfterTest(passwordPattern, passwordList);
+    }
 }
-
-
-
-
-

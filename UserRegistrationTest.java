c37a1966b8b07e0f2c4ac05009dea8bc42429a3f
vishnu <vishnu388@gmail.com>
2021-03-24 13:36:30 +0530

    [vishnu]:Added to Exception Test Case

--- UserRegistrationTest.java
+++ UserRegistrationTest.java
@@ -3,54 +3,94 @@ import org.junit.Test;
 
 public class UserRegistrationTest {
     @Test
-    public void givenFirstName_whenProper_ShouldReturnTrue() {
-        UserRegistration usr=new UserRegistration();
-        boolean result=usr.validateFirstName("Vishnu");
-        Assert.assertTrue(result);
-
+    public void givenFirstName_WhenValid_ShouldReturnTrue(){
+        String testString = "Vishnu";
+        try {
+            Assert.assertTrue(UserRegistration.validatePattern(testString, UserRegistration.firstNamePattern));
+        } catch (UserExceptions ue) {
+            ue.printStackTrace();
+        }
     }
     @Test
-    public void givenFirstName_whenProper_ShouldReturnFalse() {
-        UserRegistration usr=new UserRegistration();
-        boolean result=usr.validateFirstName("vishnu");
-        Assert.assertFalse(result);
-
+    public void givenFirstName_WhenInvalid_ShouldReturnFalse(){
+        String testString = "vi";
+        try {
+            Assert.assertFalse(UserRegistration.validatePattern(testString, UserRegistration.firstNamePattern));
+        } catch (UserExceptions ue) {
+            ue.printStackTrace();
+        }
     }
     @Test
-    public void givenLastName_whenProper_ShouldReturnTrue() {
-        UserRegistration usr=new UserRegistration();
-        boolean result=usr.validateLastName("Vardhan");
-        Assert.assertTrue(result);
-
-    }
-
-    @Test
-    public void givenLastName_whenProper_ShouldReturnFalse() {
-        UserRegistration usr=new UserRegistration();
-        boolean result=usr.validateLastName("vardhan");
-        Assert.assertFalse(result);
-
-    }
-
-    @Test
-    public void givenMob_Number_ShouldReturnTrue() {
-        UserRegistration usr =new UserRegistration();
-        boolean result=usr.validateMobileNumber("91-8106261925");
-        Assert.assertTrue(result);
+    public void givenLastName_WhenValid_ShouldReturnTrue(){
+        String testString = "Vardhan";
+        try {
+            Assert.assertTrue(UserRegistration.validatePattern(testString, UserRegistration.lastNamePattern));
+        } catch (UserExceptions ue) {
+            ue.printStackTrace();
+        }
     }
     @Test
-    public void givenMob_Number_ShouldReturnFalse() {
-        UserRegistration usr =new UserRegistration();
-        boolean result=usr.validateMobileNumber("8106261925");
-        Assert.assertFalse(result);
+    public void givenLastName_WhenInvalid_ShouldReturnFalse(){
+        String testString = "vardhan";
+        try {
+            Assert.assertFalse(UserRegistration.validatePattern(testString, UserRegistration.lastNamePattern));
+        } catch (UserExceptions ue) {
+            ue.printStackTrace();
+        }
     }
-
     @Test
-    public void givenPassword_ShouldReturnTrue() {
-        UserRegistration usr = new UserRegistration();
-        boolean result=usr.validatePassword("Vishnu@388");
-        Assert.assertTrue(result);
-
+    public void givenEmail_WhenValid_ShouldReturnTrue(){
+        String testString = "abc@yahoo.com" ;
+        try {
+            Assert.assertTrue(UserRegistration.validatePattern(testString, UserRegistration.emailPattern));
+        } catch (UserExceptions ue) {
+            ue.printStackTrace();
+        }
+    }
+    @Test
+    public void givenEmail_WhenInvalid_ShouldReturnFalse(){
+        String testString = "abc";
+        try {
+            Assert.assertFalse(UserRegistration.validatePattern(testString, UserRegistration.emailPattern));
+        } catch (UserExceptions ue) {
+            ue.printStackTrace();
+        }
+    }
+    @Test
+    public void givenMobileNumber_WhenValid_ShouldReturnTrue(){
+        String testString = "91-8106261925";
+        try {
+            Assert.assertTrue(UserRegistration.validatePattern(testString, UserRegistration.mobileNumberPattern));
+        } catch (UserExceptions ue) {
+            ue.printStackTrace();
+        }
+    }
+    @Test
+    public void givenMobileNumber_WhenInvalid_ShouldReturnFalse(){
+        String testString = "8106261925";
+        try {
+            Assert.assertFalse(UserRegistration.validatePattern(testString, UserRegistration.mobileNumberPattern));
+        } catch (UserExceptions ue) {
+            ue.printStackTrace();
+        }
+    }
+    @Test
+    public void givenPassword_WhenValid_ShouldReturnTrue(){
+        String testString = "A.Vishnu@388";
+        try {
+            Assert.assertTrue(UserRegistration.validatePattern(testString, UserRegistration.passwordPattern));
+        } catch (UserExceptions ue) {
+            ue.printStackTrace();
+        }
+    }
+    @Test
+    public void givenPassword_WhenInvalid_ShouldReturnFalse(){
+        String testString = "vishnu388";
+        try {
+            Assert.assertFalse(UserRegistration.validatePattern(testString, UserRegistration.passwordPattern));
+        } catch (UserExceptions ue) {
+            ue.printStackTrace();
+        }
     }
 
 
